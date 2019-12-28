85abc59abdcb41a83de306cbf9152f5a71c5d6f2
simon <xuzimin_g@163.com>
2019-12-28 12:18:30 +0800

    improve some code

--- src/test/java/com/simon/credit/toolkit/PdfToLongImageTest.java
+++ src/test/java/com/simon/credit/toolkit/PdfToLongImageTest.java
@@ -3,7 +3,7 @@ package com.simon.credit.toolkit;
 import java.io.File;
 import java.io.IOException;
 
-import com.simon.credit.toolkit.image.ImageVerticalSynthesizer;
+import com.simon.credit.toolkit.pdf.PdfConverter;
 
 /**
  * PDF文件转为长图片
@@ -12,15 +12,16 @@ import com.simon.credit.toolkit.image.ImageVerticalSynthesizer;
 public class PdfToLongImageTest {
 
 	public static void main(String[] args) throws IOException {
-		// File pdfFile = new File("C:\\Users\\Administrator\\Desktop\\2222.pdf");
+		File pdfFile = new File("C:\\Users\\Administrator\\Desktop\\Java\\面试\\Dubbo源码解析\\Dubbo源码解析（01）Hello,Dubbo.pdf");
 
 		// PDF文件转为图片
-		// File[] imageFiles = PdfConverter.getInstance().castToImages(pdfFile);
-		String[] imageFiles = {"d:/r1.png","d:/r2.png","d:/r3.png","d:/r4.png"};
+		File[] imageFiles = PdfConverter.getInstance().castToImages(pdfFile);
+		System.out.println(imageFiles);
+//		String[] imageFiles = {"d:/r1.png","d:/r2.png","d:/r3.png","d:/r4.png"};
 
 		// 图片合成
-		File synthesizeFile = ImageVerticalSynthesizer.getInstance().imageSynthesize(imageFiles);
-		System.out.println("=== synthesizeFile: " + synthesizeFile.getPath());
+//		File synthesizeFile = ImageVerticalSynthesizer.getInstance().imageSynthesize(imageFiles);
+//		System.out.println("=== synthesizeFile: " + synthesizeFile.getPath());
 
 		// 图片压缩
 //		String compressImage = ImageProcesser.getInstance().compress(synthesizeFile);
